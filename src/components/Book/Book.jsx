0381ea9afef1681d1984e801ad12a08a44754a88
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-08-31 01:07:00 +0600

    load data for the page

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,10 @@
+const Book = ({ book }) => {
+	const { image } = book;
+	return (
+		<div>
+			<img src={image} alt="" />
+		</div>
+	);
+};
+
+export default Book;
--- src/components/BookList/BookList.jsx
+++ src/components/BookList/BookList.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
+
 const BookList = () => {
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		fetch("books.json")
+			.then((res) => res.json())
+			.then((data) => setBooks(data));
+	}, []);
+
 	return (
 		<div>
-			<h2 className="text-5xl text-center font-bold mt-16">Books</h2>
+			<div>
+				<h2 className="text-5xl text-center font-bold mt-16">Books</h2>
+			</div>
+			<div>
+				{books.map((book) => (
+					<Book key={book.bookId} book={book}></Book>
+				))}
+			</div>
 		</div>
 	);
 };
